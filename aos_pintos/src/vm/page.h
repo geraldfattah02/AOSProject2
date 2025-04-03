482f3526bbf46d1b8c0ee820a657c7529ca48d75
geraldfattah02 <jarallahfattah@gmail.com>
2025-04-03 01:19:02 +0000

    Supplemental page table wip

---
+++ aos_pintos/src/vm/page.h
@@ -0,0 +1,17 @@
+#include "../threads/palloc.h"
+#include "../threads/malloc.h"
+#include "../lib/kernel/list.h"
+#include "../lib/debug.h"
+#include "frame.h"
+#include "../threads/synch.h"
+#include "../userprog/pagedir.h"
+#include "../threads/thread.h"
+
+struct supplemental_page_table_entry{
+    struct list_elem elem;
+
+    void* pageAdress; 
+    bool isFaulted;
+};
+
+
